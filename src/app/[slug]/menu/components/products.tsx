cb8d3419e0a585cdb6f99e0f81ab2fc34ada050d
RaferaX <rafael.larissinha@gmail.com>
2025-02-21 14:49:33 -0300

    feat: add restaurant menu page with categories and products

---
+++ src/app/[slug]/menu/components/products.tsx
@@ -0,0 +1,45 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductsProps {
+    products: Product[];
+}
+
+
+const Products = ({ products }: ProductsProps) => {
+    return ( 
+        <div className="space-y-3 px-5 py-3">
+            {products.map((product) => (
+                <Link key={product.id} href="/" className="flex items-center justify-between gap-10 border-b py-3">
+
+                    {/* ESQUERDA */}
+                    <div>
+                        <h3 className="text-sm font-medium">
+                            {product.name}
+                            <p className="line-clamp-2 text-sm text-muted-foreground">
+                                {product.description}
+                            </p>
+                            <p>{Intl.NumberFormat("pt-BR", {
+                                style: "currency",
+                                currency: "BRL",
+                            }).format(product.price)}</p>
+                        </h3>
+                    </div>
+
+                    {/* DIREITA */}
+                    <div className="relative min-h-[82px] min-w-[120px]">
+                        <Image
+                        src={product.imageUrl}
+                        alt={product.name}
+                        fill
+                        className="rounded-lg object-contain"
+                        />
+                    </div>
+                </Link>
+            ))}
+        </div>
+     );
+};
+ 
+export default Products;
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -36,9 +37,9 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
 
 export default RestaurantMenuPage;
-// http://localhost:3000/fsw-donalds/menu?consumptionMethod=dine_in
