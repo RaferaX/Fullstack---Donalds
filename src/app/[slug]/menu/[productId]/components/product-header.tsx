71b5abb3c3fbfd1a297eb6c34c3c9e3fbe794f23
RaferaX <rafael.larissinha@gmail.com>
2025-02-23 22:00:15 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -8,15 +8,15 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
-    product: Pick<Product, "name" | "imageUrl">;
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-    const router = useRouter();
-    const handleBackClick = () => router.back();
-    return ( 
-        <div className="relative h-[300px] w-full">
-        <Button
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+  return (
+    <div className="relative min-h-[300px] w-full">
+      <Button
         variant="secondary"
         size="icon"
         className="absolute left-4 top-4 z-50 rounded-full"
@@ -25,18 +25,22 @@ const ProductHeader = ({ product }: ProductHeaderProps) => {
         <ChevronLeftIcon />
       </Button>
 
-      
-        <Image src={product.imageUrl} alt={product.name} fill className="object-contain"/>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
 
-        <Button
+      <Button
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
       >
         <ScrollTextIcon />
       </Button>
-        </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default ProductHeader;
